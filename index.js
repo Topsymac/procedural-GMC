d5c2df77c0edaa860e0be4273bc326bcb1b76fe7
Topsymac <temitopsy35@gmail.com>
2022-11-17 14:10:41 +0100

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ Write a function, called dot_product which calculates in the variable ps, the do
 Write an algorithm which determines, for n pairs of given vectors, whether two vectors of given IR are orthogonal, by calling the procedure defined in the previous question. The dot product of two orthogonal vectors is zero.
 */
 
+function dotProduct(vector1, vector2) {
+    let result = 0;
+    for (let i = 0; i < vector1.length; i++) {
+        result += vector1[i] * vector2[i]
+    }
+    return result
+}
+
+for (let i = 0; i < vector.length; i++) {
+    for (let j = i + 1; j < vector.length; j++) {
+        if (dotProduct(vector[i], vector[j]) === 0) {
+            console.log(`${vector[i]} and ${vector[j]} are orthogonal `)
+        }
+    }
+}
+
 
 
 /*Second Task:
@@ -16,3 +32,21 @@ Insertion sort is a simple sorting algorithm that works the way we sort playing 
 Each time work only with the first i-1 element from of the array
 Pick element arr[i] and insert it into sorted sequence in the array from 0 to i-1.
 */
+function insertionSort(arr, n) {
+    let i,
+        key,
+        j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        /* Move elements of arr[0..i-1], that are 
+        greater than key, to one position ahead 
+        of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
